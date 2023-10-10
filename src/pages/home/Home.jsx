eabbc95b5881e7b0af4269c32ebed8a9763fe237
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-11 03:37:58 +0530

    added Top rated section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./style.scss";
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
 import Popular from './popular/Popular';
+import TopRated from './topRated/TopRated';
 
 const home = () => {
   return (
@@ -10,6 +11,7 @@ const home = () => {
       <HeroBanner/>
       <Trending />
       <Popular />
+      <TopRated />
       <div style = {{ height: 1000 }}></div>
     </div>
   )
