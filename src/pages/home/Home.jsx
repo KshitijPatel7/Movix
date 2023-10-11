91554360ca3baf71e972262c38fa98bbc06480c1
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-11 05:42:02 +0530

    updated home section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
+
 import "./style.scss";
-import HeroBanner from './heroBanner/HeroBanner';
-import Trending from './trending/Trending';
-import Popular from './popular/Popular';
-import TopRated from './topRated/TopRated';
 
-const home = () => {
-  return (
-    <div className='homePage'>
-      <HeroBanner/>
-      <Trending />
-      <Popular />
-      <TopRated />
-      <div style = {{ height: 1000 }}></div>
-    </div>
-  )
-}
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
-export default home
+const Home = () => {
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
+        </div>
+    );
+};
+
+export default Home;
